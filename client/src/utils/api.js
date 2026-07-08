e09bdc5509216971fe97933896fee60af6a6e8df
khansaabp <gazikhan747@gmail.com>
2026-07-08 22:59:07 +0530

    final2

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://new-sona-ent-4.onrender.com';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL
