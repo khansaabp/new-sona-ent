7dfe4297a2f218e9c914f9bd80eb78861108142b
khansaabp <gazikhan747@gmail.com>
2026-07-18 17:54:20 +0530

    delete invoice admin

--- client/src/pages/DashboardDeletedOrders.js
+++ client/src/pages/DashboardDeletedOrders.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import api from '../utils/api';
 import { formatCurrency, formatDateTime } from '../utils/format';
 
